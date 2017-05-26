9d1e6733936a491f8dceccca612121d6dd5dc03a
imaike <imaike@users.noreply.github.com>
2017-05-26 14:47:48 +0900

    version update

--- src/translations.js
+++ src/translations.js
@@ -9,7 +9,7 @@
 var resources = {
     'en-us': {
         "translation": {
-            "versionNumber": "Beta Version 0.9.2 Test Release (May 19, 2017)",
+            "versionNumber": "Beta Version 0.10.0 Test Release (May 31, 2017)",
             "Translation by": "",
             "Top": "Top",
             "aData": "1.Data",
@@ -307,7 +307,7 @@ var resources = {
     },
     "ja": {
         "translation": {
-            "versionNumber": "Beta Version 0.9.1 Test Release (May 19, 2017)",
+            "versionNumber": "Beta Version 0.10.0 Test Release (May 31, 2017)",
             "Translation by": "Translation by バナシック",
             "Top": "トップに戻る",
             "aData": "1.データ",
